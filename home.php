54ca30637fecc4e1431a3269ce6f656e0a72d981
unknown <Dunghuvy@gmail.com>
2024-05-28 23:11:24 +0700

    Pushing footer and header

---
+++ home.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>HOME</title>
+        
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+        <link rel="stylesheet" href="style.css">
+
+
+    </head>
+    <body>
+        <section class="header">
+            <a href="home.php" class="logo">Hotel booking</a>
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="book.php">book</a>
+            <a href="package.php">package</a>
+        </nav>
+        <div id="menu-btn" class="fas fa-bars"></div>
+</section>
+
+        
+
+
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+                <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+                <a href="#"><i class="fas fa-angle-right"></i> ask questions</a>
+                <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+                <a href="#"><i class="fas fa-angle-right"></i> privacy policy</a>
+                <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+        </div>
+        <div class="box">
+            <h3>contact info</h3>
+                <a href="#"><i class="fas fa-phone"></i> +123-456-7890 </a>
+                <a href="#"><i class="fas fa-phone"></i> +111-222-3333 </a>
+                <a href="#"><i class="fas fa-envelope"></i> shaikhanas@gmail.com </a>
+                <a href="#"><i class="fas fa-map"></i> mumbai, india - 400104 </a>
+        </div>
+
+    </div>
+    <div class="credit">create from Dung And Phi experience</div>
+</section>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="scrips.js"></script>    
+    </body>
+</html>
